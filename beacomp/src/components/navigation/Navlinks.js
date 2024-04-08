bd4a7b697630f375047e1e4d9422a421e5ee294d
tomcat <tommy.purba@dataon.com>
2024-04-08 15:10:15 +0700

    perbaiki sub menu12344

--- beacomp/src/components/navigation/Navlinks.js
+++ beacomp/src/components/navigation/Navlinks.js
@@ -1,10 +1,47 @@
 import React from "react";
 import { useState } from "react";
-import links from "./myLinks";
-
 
 export const Navlinks = () => {
   const [heading, setHeading] = useState("");
+  const links = [
+    {
+      name: "Profile",
+      submenu: true,
+      sublink: [
+        {
+          subName: "Organisasi",
+          SubNavMenu: true,
+          SubLinks: [
+            { Subhead: "test", link: "/test" },
+            { Subhead: "rest", link: "/rest" },
+            { Subhead: "best", link: "/best" },
+          ],
+        },
+        {
+          subName: "sadf",
+          SubNavMenu: true,
+          SubLinks: [
+            { Subhead: "tedsast", link: "/test" },
+            { Subhead: "redsast", link: "/rest" },
+            { Subhead: "besdast", link: "/best" },
+          ],
+        },
+        {
+          subName: "tererere",
+          SubNavMenu: true,
+          SubLinks: [
+            { Subhead: "tedsast", link: "/test" },
+            { Subhead: "redasst", link: "/rest" },
+            { Subhead: "bedasst", link: "/best" },
+          ],
+        },
+      ],
+    },
+    { name: "Peraturan", href: "/Peraturan" },
+    { name: "PPID", href: "/PPID" },
+    { name: "Berita", href: "/Berita" },
+    { name: "Pengaduan", href: "/Pengaduan" },
+  ];
 
   return (
     <>
