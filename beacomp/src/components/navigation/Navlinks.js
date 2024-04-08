41a9f7bc672ae84845b460bd1c6c2240ef1f0264
tomcat <tommy.purba@dataon.com>
2024-04-08 11:05:24 +0700

    chore: add array for sublink

--- beacomp/src/components/navigation/Navlinks.js
+++ beacomp/src/components/navigation/Navlinks.js
@@ -1,79 +1,54 @@
 import React from "react";
-import Link from "next/link";
 
 export const Navlinks = () => {
   const links = [
     {
       name: "Profile",
       submenu: true,
-      subNavlink: {
-        subName: "Organisasi",
-        SubNavMenu: true,
-        SubLinks: [
-          { Subhead: "test", link: "/test" },
-          { Subhead: "rest", link: "/rest" },
-          { Subhead: "best", link: "/best" },
-        ],
-      },
+      sublink: [
+        {
+          subName: "Organisasi",
+          SubNavMenu: true,
+          SubLinks: [
+            { Subhead: "test", link: "/test" },
+            { Subhead: "rest", link: "/rest" },
+            { Subhead: "best", link: "/best" },
+          ],
+        },
+      ],
     },
-    {
-      name: "Peraturan",
-      submenu: true,
-      subNavlink: {
-        subName: "Organisasi",
-        SubNavMenu: true,
-        SubLinks: [
-          { Subhead: "test", link: "/test" },
-          { Subhead: "rest", link: "/rest" },
-          { Subhead: "best", link: "/best" },
-        ],
-      },
-    },
+    { name: "Peraturan", href: "/Peraturan" },
     { name: "PPID", href: "/PPID" },
-    {
-      name: "Berita",
-      submenu: true,
-      subNavlink: {
-        subName: "Organisasi",
-        SubNavMenu: true,
-        SubLinks: [
-          { Subhead: "test", link: "/test" },
-          { Subhead: "rest", link: "/rest" },
-          { Subhead: "best", link: "/best" },
-        ],
-      },
-    },
+    { name: "Berita", href: "/Berita" },
     { name: "Pengaduan", href: "/Pengaduan" },
   ];
 
   return (
     <>
-      {links.map((link, index) => (
-        <div key={index} className="px-3 text-left md:cursor-pointer">
-          <h1 className="py-6">{link.name}</h1>
-          {link.submenu && (
+      {links.map((links) => (
+        <div className="px-3 text-left md:cursor-pointer">
+          <h1 className="py-6">{links.name}</h1>
+          {links.submenu && (
             <div>
               <div>
                 <div>
-                  {link.subNavlink.SubLinks.map((SubmenuLink, index) => (
-                    <div key={index}>
+                  {links.sublink.map((mySubLinks) => (
+                    <div>
                       <h3 className="text-lg font-semibold">
-                        {SubmenuLink.subName}
+                        {mySubLinks.subName}
                       </h3>
-                      {SubmenuLink.SubNavMenu && (
+                      {mySubLinks.SubNavMenu && (
                         <div>
                           <div>
                             <div>
-                              {SubmenuLink.SubLinks.map((SubLink, index) => (
-                                <div
-                                  key={index}
-                                  className="text-sm text-gray-500 my-2.5 "
-                                >
-                                  <Link href={SubLink.link}>
-                                    <a className="hover:text-indigo-300">
-                                      {SubLink.Subhead}
-                                    </a>
-                                  </Link>
+                              {mySubLinks.SubLinks.map((SubmenuLink) => (
+                                <div className="text-sm text-gray-500 my-2.5 ">
+                                  <a
+                                    className="hover:text-indigo-300"
+                                    href={SubmenuLink.link}
+                                  >
+                                    {SubmenuLink.Subhead}
+                                  </a>
                                 </div>
                               ))}
                             </div>
@@ -85,11 +60,6 @@ export const Navlinks = () => {
                 </div>
               </div>
             </div>
-          )}
-          {!link.submenu && (
-            <Link href={link.href}>
-              <a className="hover:text-indigo-300">{link.name}</a>
-            </Link>
           )}
         </div>
       ))}
