02bdd7e489d80ae3061fbe6e26c61dcf7a54d2df
tomcat <tommy.purba@dataon.com>
2024-04-08 11:33:30 +0700

    feat: add hidden menu

--- beacomp/src/components/navigation/Navlinks.js
+++ beacomp/src/components/navigation/Navlinks.js
@@ -44,24 +44,27 @@ export const Navlinks = () => {
   return (
     <>
       {links.map((links) => (
-        <div className="px-3 text-left md:cursor-pointer">
+        <div className="px-3 text-left md:cursor-pointer group">
           <h1 className="py-6">{links.name}</h1>
           {links.submenu && (
             <div>
-              <div className="absolute top-20">
+              <div className="absolute top-20 hidden group-hover:block hover:block">
                 <div className="py-3">
                   <div className="w-4 h-4 left-3 absolute mt-1 bg-white rotate-45"></div>
                 </div>
                 <div className="bg-white p-3.5">
                   {links.sublink.map((mySubLinks) => (
                     <div>
-                      <h5 className="text-lg font-semibold">
+                      <h6 className="text-lg hover:to-blue-950">
                         {mySubLinks.subName}
-                      </h5>
+                      </h6>
                       {mySubLinks.SubNavMenu && (
                         <div>
-                          <div>
-                            <div>
+                          <div className="absolute right-20">
+                            <div className="py-3">
+                              <div className="w-4 h-4 left-3 absolute mt-1 bg-white rotate-45"></div>
+                            </div>
+                            <div className="bg-white p-3.5">
                               {mySubLinks.SubLinks.map((SubmenuLink) => (
                                 <div className="text-sm text-gray-500 my-2.5 ">
                                   <a
