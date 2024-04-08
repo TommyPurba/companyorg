1cb9851c45aa0f67c99b83918cc93b255b387747
tomcat <tommy.purba@dataon.com>
2024-04-08 15:19:45 +0700

    perbaiki sub menu12344

--- beacomp/src/components/navigation/Navlinks.js
+++ beacomp/src/components/navigation/Navlinks.js
@@ -46,60 +46,70 @@ export const Navlinks = () => {
   return (
     <>
       {links.map((links, index) => (
-        <div key={index} className="px-3 text-left md:cursor-pointer group">
-          <h1 className="py-6">{links.name}</h1>
-          {links.submenu && (
-            <div>
-              <div className="absolute top-20 hidden group-hover:block hover:block">
-                <div className="py-3">
-                  <div className="w-4 h-4 left-3 absolute mt-1 bg-gray-400 rotate-45"></div>
-                </div>
-                <div className="bg-gray-400 p-3.5 md:w-32">
-                  {links.sublink.map((mySubLinks, index) => (
-                    <div key={index}>
-                      <p
-                        className="hover:text-white"
-                        onClick={() => setHeading(mySubLinks.subName)}
-                      >
-                        {mySubLinks.subName}
-                      </p>
-                      {mySubLinks.SubNavMenu && (
-                        <div>
-                          <div
-                            className={`${
-                              heading == mySubLinks.subName
-                                ? "md:block"
-                                : "hidden"
-                            }`}
-                          >
-                            <div className="bg-gray-400 p-3.5">
-                              {mySubLinks.SubLinks.map((SubmenuLink, index) => (
-                                <div
-                                  key={index}
-                                  className="text-sm text-gray-500 "
-                                >
-                                  <a
-                                    key={index}
-                                    className="hover:text-indigo-300"
-                                    href={SubmenuLink.link}
-                                  >
-                                    {SubmenuLink.Subhead}
-                                  </a>
-                                </div>
-                              ))}
+        <div>
+          <div key={index} className="px-3 text-left md:cursor-pointer group">
+            <h1 className="py-6">{links.name}</h1>
+            {links.submenu && (
+              <div>
+                <div className="absolute top-20 hidden group-hover:block hover:block">
+                  <div className="py-3">
+                    <div className="w-4 h-4 left-3 absolute mt-1 bg-gray-400 rotate-45"></div>
+                  </div>
+                  <div className="bg-gray-400 p-3.5 md:w-32">
+                    {links.sublink.map((mySubLinks, index) => (
+                      <div key={index}>
+                        <p
+                          className="hover:text-white"
+                          onClick={() => setHeading(mySubLinks.subName)}
+                        >
+                          {mySubLinks.subName}
+                        </p>
+                        {mySubLinks.SubNavMenu && (
+                          <div>
+                            <div
+                              className={`${
+                                heading == mySubLinks.subName ? "md:" : "hidden"
+                              }`}
+                            >
+                              <div className="bg-gray-400 p-3.5">
+                                {mySubLinks.SubLinks.map(
+                                  (SubmenuLink, index) => (
+                                    <div
+                                      key={index}
+                                      className="text-sm text-gray-500 "
+                                    >
+                                      <a
+                                        key={index}
+                                        className="hover:text-indigo-300"
+                                        href={SubmenuLink.link}
+                                      >
+                                        {SubmenuLink.Subhead}
+                                      </a>
+                                    </div>
+                                  )
+                                )}
+                              </div>
                             </div>
                           </div>
-                        </div>
-                      )}
-                    </div>
-                  ))}
+                        )}
+                      </div>
+                    ))}
+                  </div>
                 </div>
               </div>
-            </div>
-          )}
-        </div>
+            )}
+          </div>
 
-        //  mobile
+          <div>
+            {links.sublink.map((sLinks, index) => (
+              <div>
+                <div>
+                  <p className="py-4 pl-7 md:pr-0 pr-5">{sLinks.subName}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       ))}
     </>
   );
