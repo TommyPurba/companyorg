9b0d84e60e3f00198e204181dee899ec07464d8d
tomcat <tommy.purba@dataon.com>
2024-04-08 11:27:00 +0700

    feat: add another submenu

--- beacomp/src/components/navigation/Navlinks.js
+++ beacomp/src/components/navigation/Navlinks.js
@@ -15,6 +15,24 @@ export const Navlinks = () => {
             { Subhead: "best", link: "/best" },
           ],
         },
+        {
+          subName: "Organisasi",
+          SubNavMenu: true,
+          SubLinks: [
+            { Subhead: "test", link: "/test" },
+            { Subhead: "rest", link: "/rest" },
+            { Subhead: "best", link: "/best" },
+          ],
+        },
+        {
+          subName: "Organisasi",
+          SubNavMenu: true,
+          SubLinks: [
+            { Subhead: "test", link: "/test" },
+            { Subhead: "rest", link: "/rest" },
+            { Subhead: "best", link: "/best" },
+          ],
+        },
       ],
     },
     { name: "Peraturan", href: "/Peraturan" },
@@ -30,13 +48,16 @@ export const Navlinks = () => {
           <h1 className="py-6">{links.name}</h1>
           {links.submenu && (
             <div>
-              <div>
-                <div>
+              <div className="absolute top-20">
+                <div className="py-3">
+                  <div className="w-4 h-4 left-3 absolute mt-1 bg-white rotate-45"></div>
+                </div>
+                <div className="bg-white p-3.5">
                   {links.sublink.map((mySubLinks) => (
                     <div>
-                      <h3 className="text-lg font-semibold">
+                      <h5 className="text-lg font-semibold">
                         {mySubLinks.subName}
-                      </h3>
+                      </h5>
                       {mySubLinks.SubNavMenu && (
                         <div>
                           <div>
